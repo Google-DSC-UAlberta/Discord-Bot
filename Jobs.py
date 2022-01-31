15ab845b32484b2dfb253b48050fecf9fe7045de
Kelly Shih <kshih@ualberta.ca>
2022-01-31 12:41:01 -0700

    delete all job entries

--- Jobs.py
+++ Jobs.py
@@ -5,6 +5,14 @@ connection=sqlite3.connect("information.db")
 
 cur=connection.cursor()
 
+
+#function: delete all saved jobs from db
+def rem_all():
+    cur.execute("DELETE FROM jobs;")
+    connection.commit()
+    connection.close
+
+
 # Making the table Sorted_data
 # Initial Information are 
 # 1) Title of Job
@@ -13,9 +21,16 @@ cur=connection.cursor()
 # 4) URL 
 
 cur.execute('''CREATE TABLE if not exists jobs( Title text, Company text, Location text , URL text)''')   
+connection.commit()
+
+#display columns
+data = cur.execute("SELECT * FROM jobs;")
+for column in data.description:
+    print(column[0])
 
 # Currently the table is empty 
-
 info_display=cur.fetchall()
 
 print(info_display)
+
+connection.close()
