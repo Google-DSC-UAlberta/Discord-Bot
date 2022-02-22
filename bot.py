ed92506ee14e19e77b656fd5fd2af08b6e9ea328
zhpeng811 <zhpeng@ualberta.ca>
2022-02-22 12:56:40 -0700

    implement basic logic for user based notification

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 
 # https://discordpy.readthedocs.io/en/stable/api.html#
 import discord
+from discord.ext import tasks
 
 import time
 import asyncio
@@ -15,27 +16,42 @@ load_dotenv()
 
 TOKEN = os.getenv("API_TOKEN")
 
-interval = int(os.getenv("TIMER_INTERVAL"))
-
 class GDSCJobClient(discord.Client):
     def __init__(self):
         super().__init__()
-        self.timer_flag = False
         self.valid_messages = {
             "test": "Welcome to UofA GDSC!",
             "code": "```python\n print('showing how python snippets can be displayed on Discord!')```"
         }
         #use whichever command works with your laptop
-        with open('Discord-Bot\greetings.json', 'r') as f:
-        #with open('greetings.json', 'r') as f:
+        with open('greetings.json', 'r') as f:
             global greetings
             greetings = json.load(f)
-        global db
-        db = Database()
-
-    async def on_ready(self):
-        print(f'{client.user} has connected to Discord!')
+        self.db = Database()
+        self.tasks = {}
     
+    async def on_ready(self):
+        print(f'{self.user} has connected to Discord!')
+    
+    async def notification(self, message):
+        # TODO: replace with actual job posting
+        await message.reply("Hello World!")
+    
+    def task_launcher(self, message, **interval):
+        new_task = tasks.loop(**interval)(self.notification)
+        self.tasks[message.author.id] = new_task
+        new_task.start(message)
+    
+    async def manage_notification(self, message):
+        # TODO: retrieve interval from database based on message.author.id
+        interval = 1 # in minutes
+        if message.author.id in self.tasks:
+            await message.reply(f"Hi {message.author.name}, You will stop being notified for job postings!")
+            self.tasks[message.author.id].cancel()
+        else:
+            self.task_launcher(message, minutes=interval)
+            await message.reply(f"Hi {message.author.name}, you will start being notified for new job postings every {interval} minutes!")
+
     async def on_message(self, message):
         # a bot message also count as a message, make sure we don't handle bot's messages
         if message.author == self.user:
@@ -44,35 +60,18 @@ class GDSCJobClient(discord.Client):
         content = message.content
         if content in self.valid_messages:
             await message.reply(self.valid_messages[content])
-    
-        if content == "timer":
-            if self.timer_flag:
-                await message.reply("You can only start one timer at a time!")
-            else:
-                await message.reply(f"Timer have been started! It will display the current time every {interval} seconds!")
-                self.timer_flag = True
-                while self.timer_flag:
-                    await message.channel.send(f"The current time is: {time.ctime()}")
-                    # this await on asyncio.sleep() is very important, try take it out and see what happens...
-                    # Note: you will get a RuntimeWarning from asyncio without this await keyword below
-                    await asyncio.sleep(interval)
-        elif content == "stop":
-            if self.timer_flag:
-                await message.reply("Timer have been stopped!")
-                self.timer_flag = False
-            else:
-                await message.reply("Timer is not started! Enter \"timer\" to start a timer")
+        elif content.lower() == "notify":
+            await self.manage_notification(message)
         elif content.lower() == "hello":
             await message.reply(random.choices(greetings['Hello'])[0])
         elif content.lower() == "bye" or content.lower() == "goodbye": 
             await message.reply(random.choices(greetings['Bye'])[0])
         elif content == "db":
-            await message.reply(db.check_if_user_exist(message.author.id))
+            await message.reply(self.db.check_if_user_exist(message.author.id))
         elif content.lower() == "jobs":
-            
-            if db.check_if_user_exist(message.author.id):
-                keywords_location = db.get_keywords_and_location(message.author.id)
-                jobs = db.get_jobs(keywords_location['job_keywords'], keywords_location['location'])
+            if self.db.check_if_user_exist(message.author.id):
+                keywords_location = self.db.get_keywords_and_location(message.author.id)
+                jobs = self.db.get_jobs(keywords_location['job_keywords'], keywords_location['location'])
 
                 #Right now there is a 4000 message limit
                 '''
