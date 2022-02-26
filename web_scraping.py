cdee72b111537e876b2038e5298948f72fa12228
FaiyadRahman <faiyad.r@yahoo.com>
2022-02-26 06:37:37 -0700

    Linkdedin Job Data
    Added functions to scrape job data from linkedin

--- web_scraping.py
+++ web_scraping.py
@@ -23,7 +23,7 @@ def html_code(url):
     # return html code
     return (soup)
 
-# return job titles
+# return job titles from indeed
 def job_title(soup, page):
     # find all tags with job titles
     parent = soup.find("div", class_ = "mosaic-zone", id  = "mosaic-zone-jobcards")
@@ -35,7 +35,7 @@ def job_title(soup, page):
     
     return titles
 
-# return company names
+# return company names from indeed
 def company_names(soup,page):
     # find the Html tag
     # with find()
@@ -48,7 +48,7 @@ def company_names(soup,page):
         
     return names
 
-# return company locations
+# return company locations from indeed
 def company_locations(soup,page):
     # find the Html tag
     # with find()
@@ -61,7 +61,7 @@ def company_locations(soup,page):
         
     return locations
 
-# return company urls  
+# return company urls from indeed
 def company_urls(soup,page):
     urls = []
     parent = soup.find("div", class_="mosaic-zone", id="mosaic-zone-jobcards")
@@ -70,6 +70,48 @@ def company_urls(soup,page):
     for link in tags:
         urls.append("https://ca.indeed.com" + link["href"])
 
+    return urls
+
+# return job titles from linkedin
+def job_title_linkedin(soup):
+    parent = soup.find("ul", class_ = "jobs-search__results-list")
+    titles = parent.findChildren("h3", class_= "base-search-card__title")
+
+    # get the text from each job title tag
+    for i in range(len(titles)):
+        titles[i] = titles[i].text.strip()
+    
+    return titles
+
+# return company names from linkedin
+def company_names_linkedin(soup):
+    parent = soup.find("ul", class_ = "jobs-search__results-list")
+    names = parent.findChildren("h4", class_ = "base-search-card__subtitle")
+
+    for i in range(len(names)):
+        names[i] = names[i].text.strip()
+
+    return names
+
+# return company locations from linkedin
+def company_locations_linkedin(soup):
+    parent = soup.find("ul", class_ = "jobs-search__results-list")
+    locations = parent.findChildren("span", class_ = "job-search-card__location")
+
+    for i in range(len(locations)):
+        locations[i] = locations[i].text.strip()
+
+    return locations
+    
+# return company urls from linkedin
+def company_urls_linkedin(soup):
+    urls = []
+    parent = soup.find("ul", class_ = "jobs-search__results-list")
+    tags = parent.findChildren("a", class_ = "base-card__full-link", href = True)
+
+    for link in tags:
+        urls.append(link["href"])
+    
     return urls
 
 # driver nodes/main function
@@ -85,6 +127,7 @@ if __name__ == "__main__":
     locations_company = []
     job_urls = []
     
+    # INDEED JOBS
     while True:
         # change 'start' every iteration to go to next page
         url = "https://ca.indeed.com/jobs?q=" + job + "&l=" + location + "&start=" + str(page)       
@@ -101,7 +144,16 @@ if __name__ == "__main__":
         # get info from the first 5 pages
         if page == 50:
             break  
-        
+    
+    # LINKEDIN JOBS
+    url = "https://www.linkedin.com/jobs/search?keywords=" + job + "&location=" + location
+    soup = html_code(url)
+
+    jobtitles += job_title_linkedin(soup)
+    names_company += company_names_linkedin(soup)
+    locations_company += company_locations_linkedin(soup)
+    job_urls += company_urls_linkedin(soup)
+    
     # creating a dataframe with all the information 
     df = pd.DataFrame()
     df['Title'] = jobtitles
