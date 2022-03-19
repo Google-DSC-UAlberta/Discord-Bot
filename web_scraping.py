c0d4a90fc2e867ae38a6a1bd75f65e112a9e4c1c
zhpeng811 <zhpeng@ualberta.ca>
2022-03-18 19:27:52 -0600

    trying with a different user agent

--- web_scraping.py
+++ web_scraping.py
@@ -8,7 +8,7 @@ from urllib.parse import quote
 db = Database()
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36"
 }
 
 # user define function
